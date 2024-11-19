5d40b1a0b7bfd529246b0d4d042a8fbb08a6e776
gawkyerim <jh19930@naver.com>
2024-11-19 16:16:42 +0900

    first-commit-samick

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.visual .con').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow:$('.prev'),
+    nextArrow:$('.next'),
+  });
+})
